39c67d06f8cd98c7252baa79b1fade0472a2d5c4
mblicharz <maciej.blicharz@protonmail.com>
2022-12-02 19:36:54 +0100

    day 2

---
+++ day2/solution.py
@@ -0,0 +1,59 @@
+"""
+A(X) - Rock
+B(Y) - Paper
+C(Z) - Scissors
+"""
+mapping = {
+    "X": "A",
+    "Y": "B",
+    "Z": "C",
+}
+shape_points = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+}
+defeats = {
+    "A": "C",
+    "B": "A",
+    "C": "B",
+}
+
+with open("input.txt") as file:
+    input = [line.split(" ") for line in file.read().splitlines()]
+
+points = 0
+
+for opponent_shape, player_shape in input:
+    points += shape_points[mapping[player_shape]]
+    if defeats[mapping[player_shape]] == opponent_shape:
+        points += 6
+    if mapping[player_shape] == opponent_shape:
+        points += 3
+
+print(points)  # part 1
+
+
+"""
+X - lose
+Y - draw
+Z - win
+"""
+
+loses = {
+    "C": "A",
+    "A": "B",
+    "B": "C",
+}
+
+points = 0
+
+for opponent_shape, expected_result in input:
+    if expected_result == "X":
+        points += shape_points[defeats[opponent_shape]]
+    if expected_result == "Y":
+        points += shape_points[opponent_shape] + 3
+    if expected_result == "Z":
+        points += shape_points[loses[opponent_shape]] + 6
+
+print(points)  # part 2
