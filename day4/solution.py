46e3d6d06d657affc1f35bddf7bccd2f9843cce9
mblicharz <maciej.blicharz@protonmail.com>
2022-12-04 16:53:26 +0100

    day 4

---
+++ day4/solution.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+
+import aocd
+
+
+class AssignmentsPair:
+    first: "AssignmentRange"
+    second: "AssignmentRange"
+
+    @dataclass
+    class AssignmentRange:
+        min: int
+        max: int
+
+    def __new__(
+        cls,
+        first_assignment: list[int],
+        second_assignment: list[int],
+    ) -> "AssignmentsPair":
+        pair = object.__new__(cls)
+        pair.first = cls.AssignmentRange(*first_assignment)
+        pair.second = cls.AssignmentRange(*second_assignment)
+
+        return pair
+
+    def is_repeated(self) -> bool:
+        return (
+            (self.first.min <= self.second.min) and (self.second.max <= self.first.max)
+        ) or (
+            (self.second.min <= self.first.min) and (self.first.max <= self.second.max)
+        )
+
+    def is_overlapped(self) -> bool:
+        return (self.first.min <= self.second.min <= self.first.max) or (
+            self.second.min <= self.first.min <= self.second.max
+        )
+
+
+input = aocd.get_data(year=2022, day=4).splitlines()
+
+pairs = []
+for line in input:
+    pairs.append(
+        AssignmentsPair(
+            *[[int(x) for x in pair.split("-")] for pair in line.split(",")]
+        )
+    )
+
+repeated_assignments = 0
+overlapped_assignments = 0
+
+for pair in pairs:
+    if pair.is_repeated():
+        repeated_assignments += 1
+    if pair.is_overlapped():
+        overlapped_assignments += 1
+
+print(repeated_assignments)  # part 1
+print(overlapped_assignments)  # part 2
